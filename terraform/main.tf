1893b7a6cb37d2ae97de482d675fa8d17c7e3b84
Shankarraghuraman <rshankarraghu@gmail.com>
2025-07-19 14:45:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,9 +23,8 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
 }
 
 resource "aws_ecs_task_definition" "usermgmt_task" {
@@ -35,7 +34,7 @@ resource "aws_ecs_task_definition" "usermgmt_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
