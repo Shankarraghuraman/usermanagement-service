5f9729a56da484b66f384d08a9727e18507e3ae0
Shankarraghuraman <rshankarraghu@gmail.com>
2025-07-20 04:09:14 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,34 @@
-# variables.tf
-
 variable "aws_region" {
-  description = "AWS region"
   type        = string
+  description = "AWS region"
 }
 
 variable "vpc_id" {
-  description = "Existing VPC ID"
   type        = string
+  description = "VPC ID"
 }
 
 variable "public_subnets" {
-  description = "List of existing public subnet IDs"
   type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "List of existing private subnet IDs"
-  type        = list(string)
+  description = "List of public subnet IDs"
 }
 
 variable "ecr_registry" {
-  description = "ECR registry URI"
   type        = string
+  description = "ECR registry URL"
 }
 
 variable "ecr_repository" {
-  description = "ECR repository name"
   type        = string
+  description = "ECR repository name"
 }
 
 variable "image_tag" {
-  description = "Docker image tag"
   type        = string
+  description = "Image tag to deploy"
 }
 
 variable "ecs_task_execution_role_name" {
-  description = "Existing IAM role name for ECS task execution"
   type        = string
+  description = "Name of the ECS task execution IAM role"
 }
