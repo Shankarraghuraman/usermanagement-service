81c2ccfa68de8eb042ab4d2de3afd246a8771176
Shankarraghuraman <rshankarraghu@gmail.com>
2025-07-20 01:41:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,139 +1,75 @@
+# main.tf
+
 provider "aws" {
   region = var.aws_region
 }
 
-# --- IAM Roles ---
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+data "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  id    = var.private_subnets[count.index]
 }
 
-# --- ECS Cluster ---
-resource "aws_ecs_cluster" "usermgmt_cluster" {
+data "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  id    = var.public_subnets[count.index]
+}
+
+resource "aws_ecs_cluster" "this" {
   name = "usermgmt-cluster"
 }
 
-# --- Log Group for ECS ---
-resource "aws_cloudwatch_log_group" "ecs_log_group" {
-  name              = "/ecs/usermgmt"
-  retention_in_days = 7
+
+data "aws_iam_role" "ecs_task_execution" {
+  name = var.ecs_task_execution_role_name
 }
 
-# --- Task Definition ---
-resource "aws_ecs_task_definition" "usermgmt_task" {
+resource "aws_ecs_task_definition" "this" {
   family                   = "usermgmt-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode            = "awsvpc"
+  cpu                     = "256"
+  memory                  = "512"
+  execution_role_arn      = data.aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([{
-    name      = "usermgmt"
-    image     = "${var.ecr_registry}/${var.ecr_repository}:${var.image_tag}"
-    essential = true
-    portMappings = [{
-      containerPort = 8080
-      hostPort      = 8080
-    }]
-    logConfiguration = {
-      logDriver = "awslogs"
-      options = {
-        awslogs-group         = "/ecs/usermgmt"
-        awslogs-region        = var.aws_region
-        awslogs-stream-prefix = "ecs"
-      }
+  container_definitions = jsonencode([
+    {
+      name      = "usermgmt",
+      image     = "${var.ecr_registry}/${var.ecr_repository}:${var.image_tag}",
+      essential = true,
+      portMappings = [
+        {
+          containerPort = 8080,
+          hostPort      = 8080
+        }
+      ]
     }
-  }])
+  ])
 }
 
-# --- ECS Service ---
-resource "aws_ecs_service" "usermgmt_service" {
+resource "aws_ecs_service" "this" {
   name            = "usermgmt-service"
-  cluster         = aws_ecs_cluster.usermgmt_cluster.id
-  task_definition = aws_ecs_task_definition.usermgmt_task.arn
-  desired_count   = 1
+  cluster         = aws_ecs_cluster.this.id
   launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = 1
 
   network_configuration {
     subnets         = var.private_subnets
-    security_groups = [aws_security_group.ecs_service_sg.id]
     assign_public_ip = false
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.ecs_target.arn
-    container_name   = "usermgmt"
-    container_port   = 8080
-  }
-
-  depends_on = [
-    aws_lb_listener.ecs_listener
-  ]
-}
-
-# --- ALB + Listener + Target Group ---
-resource "aws_lb" "ecs_alb" {
-  name               = "usermgmt-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = var.public_subnets
-}
-
-resource "aws_lb_target_group" "ecs_target" {
-  name     = "ecs-target-group"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "ip"
-  health_check {
-    path = "/"
-    interval = 30
-    timeout  = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+    security_groups = [aws_security_group.ecs_tasks.id]
   }
 }
 
-resource "aws_lb_listener" "ecs_listener" {
-  load_balancer_arn = aws_lb.ecs_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_target.arn
-  }
-}
-
-# --- Security Groups ---
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP access to ALB"
+resource "aws_security_group" "ecs_tasks" {
+  name        = "ecs-tasks-sg"
+  description = "Allow ECS Fargate tasks"
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
@@ -141,24 +77,11 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "ecs_service_sg" {
-  name        = "ecs-service-sg"
-  description = "Allow traffic from ALB"
-  vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 8080,
+    to_port     = 8080,
+    protocol    = "tcp",
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
