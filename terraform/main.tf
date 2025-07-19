d1162d572aa2006220e42a618dd95e22fe126e2c
Shankarraghuraman <rshankarraghu@gmail.com>
2025-07-20 04:30:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,32 @@
-### main.tf
-
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
-
-data "aws_subnet" "public" {
-  count = length(var.public_subnets)
-  id    = var.public_subnets[count.index]
-}
-
-resource "aws_ecs_cluster" "this" {
-  name = "usermgmt-cluster"
-}
-
-data "aws_iam_role" "ecs_task_execution" {
+data "aws_iam_role" "ecs_execution_role" {
   name = var.ecs_task_execution_role_name
 }
 
-resource "aws_ecs_task_definition" "this" {
+resource "aws_ecs_cluster" "main" {
+  name = "usermgmt-cluster"
+}
+
+resource "aws_ecs_task_definition" "usermgmt" {
   family                   = "usermgmt-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = "256"
-  memory                  = "512"
-  execution_role_arn      = data.aws_iam_role.ecs_task_execution.arn
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = data.aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "usermgmt",
-      image     = "${var.ecr_registry}/${var.ecr_repository}:${var.image_tag}",
-      essential = true,
+      name      = "usermgmt"
+      image     = "${var.ecr_registry}/${var.ecr_repository}:${var.image_tag}"
+      essential = true
       portMappings = [
         {
-          containerPort = 8080,
-          hostPort      = 8080,
+          containerPort = 8080
+          hostPort      = 8080
           protocol      = "tcp"
         }
       ]
@@ -45,12 +34,12 @@ resource "aws_ecs_task_definition" "this" {
   ])
 }
 
-resource "aws_ecs_service" "this" {
+resource "aws_ecs_service" "usermgmt" {
   name            = "usermgmt-service"
-  cluster         = aws_ecs_cluster.this.id
-  launch_type     = "FARGATE"
-  task_definition = aws_ecs_task_definition.this.arn
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.usermgmt.arn
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
     subnets          = var.public_subnets
@@ -61,20 +50,20 @@ resource "aws_ecs_service" "this" {
 
 resource "aws_security_group" "ecs_tasks" {
   name        = "ecs-tasks-sg"
-  description = "Allow ECS Fargate tasks"
+  description = "Allow HTTP traffic to ECS tasks"
   vpc_id      = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
